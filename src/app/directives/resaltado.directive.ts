252dab53f8c8084e468f2cc4ba1fe34cd798405f
Angel <angelj@edivers.mx>
2018-08-20 18:55:11 -0500

    Se agrego directiva

--- src/app/directives/resaltado.directive.ts
+++ src/app/directives/resaltado.directive.ts
@@ -1,10 +1,11 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
 export class ResaltadoDirective {
-
-  constructor() { }
-
+  constructor( private el: ElementRef ) {
+    console.log('Directiva llamada');
+    el.nativeElement.style.backgroundColor = 'yellow'; 
+   }
 }
